c96d778eb4e0a6d2fe1c97e019c43c91f9d486f6
Ilja Savkiv <iljasavkiv@MacBook-Pro-von-Ilja-2.local>
2025-05-05 16:48:16 +0200

    added generateCircleSVG() and generateCrossSVG()

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function render() {
             let symbol = '';
 
             if (fields[index] === 'circle') {
-                symbol = 'o';
+                symbol = generateCircleSVG();
             } else if (fields[index] === 'cross') {
-                symbol = 'x';
+                symbol = generateCrossSVG();
             }
             html += `<td>${symbol}</td>`;
         }
@@ -41,3 +41,55 @@ function render() {
     contentDiv.innerHTML = html;
 }
 
+function generateCircleSVG() {
+    return `
+        <svg width="70" height="70" viewBox="0 0 70 70">
+            <circle cx="35" cy="35" r="30"
+                    stroke="#00B0EF"
+                    stroke-width="5"
+                    fill="none"
+                    stroke-dasharray="188.4"
+                    stroke-dashoffset="188.4">
+                <animate attributeName="stroke-dashoffset"
+                         from="188.4"
+                         to="0"
+                         dur="250ms"
+                         fill="freeze"
+                         repeatCount="1"/>
+            </circle>
+        </svg>
+    `;
+}
+
+function generateCrossSVG() {
+    return `
+        <svg width="90" height="90" viewBox="0 0 70 70">
+            <line x1="20" y1="20" x2="50" y2="50"
+                  stroke="#FFC000"
+                  stroke-width="5"
+                  stroke-linecap="round"
+                  stroke-dasharray="42.43"
+                  stroke-dashoffset="42.43">
+                <animate attributeName="stroke-dashoffset"
+                         from="42.43"
+                         to="0"
+                         dur="250ms"
+                         fill="freeze"
+                         repeatCount="1"/>
+            </line>
+            <line x1="50" y1="20" x2="20" y2="50"
+                  stroke="#FFC000"
+                  stroke-width="5"
+                  stroke-linecap="round"
+                  stroke-dasharray="42.43"
+                  stroke-dashoffset="42.43">
+                <animate attributeName="stroke-dashoffset"
+                         from="42.43"
+                         to="0"
+                         dur="250ms"
+                         fill="freeze"
+                         repeatCount="1"/>
+            </line>
+        </svg>
+    `;
+}
